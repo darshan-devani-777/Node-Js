07f5fc90113d92950cf7fef7a344df99ddd2b691
darshan-devani-777 <darshan.devani@tecocraft.com>
2025-09-10 18:27:27 +0530

    GraphQL

--- GraphQL/src/helpers/response.js
+++ GraphQL/src/helpers/response.js
@@ -1,6 +1,6 @@
-const Response = (status, code, message, data = null) => {
+const buildResponse = (status, code, message, data = null) => {
     return { status, code, message, data };
   };
   
-  module.exports = Response;
+  module.exports = buildResponse;
   
